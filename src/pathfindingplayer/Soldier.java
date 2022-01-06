07956f657529d3e7e421f019f136708501b11890
CyrilSharma <sharma.cyril@gmail.com>
2022-01-05 21:07:08 -0500

    soldiers know clump around miners

--- src/pathfindingplayer/Soldier.java
+++ src/pathfindingplayer/Soldier.java
@@ -6,7 +6,10 @@ import java.util.*;
 public class Soldier extends Unit {
     int counter = 0;
     int archon_index = -1;
-    float attraction = 1 / 100;
+    double s_attraction = 1/2;
+    double m_attraction = 10;
+    double s_repulsion = 2;
+    double m_repulsion = 1/10;
 
     MapLocation target;
     int[] exploratoryDir = getExploratoryDir();
@@ -18,13 +21,9 @@ public class Soldier extends Unit {
     public void run() throws GameActionException {
         rc.setIndicatorString("archon_found: " + archon_found);
         if (isExploring()){
-            moveInDirection(exploratoryDir);
-            if (counter % 2 == 0) {
-                senseArchon();
-            }
-            if (adjacentToEdge()) {
-                exploratoryDir = getExploratoryDir();
-            }
+            Direction dir = friendlyDir();
+            rc.setIndicatorString("dir: " + dir);
+            moveInDirection(friendlyDir());
         }
         else if (archon_found) {
             huntArchon();
@@ -100,28 +99,79 @@ public class Soldier extends Unit {
         fuzzyMove(target);
     }
 
-    public void stayNearFriendly() {
+    public Direction friendlyDir() throws GameActionException {
+        Direction d = Direction.NORTH; // placeholder
         RobotInfo[] friendlyRobos = rc.senseNearbyRobots(9, rc.getTeam());
         MapLocation loc = rc.getLocation();
         int num = friendlyRobos.length;
-        float cx = 0;
-        float cy = 0;
-        float dx2 = 0;
-        float dy2 = 0;
+        double cx = 0;
+        double cy = 0;
+        double dx2 = 0;
+        double dy2 = 0;
 
         for (RobotInfo robot: friendlyRobos) {
-            cx += robot.location.x;
-            cy += robot.location.y;
-            // absolute value
-            if ((Math.abs(robot.location.x - loc.x) + Math.abs(robot.location.y - loc.y)) < 2) {
-                dx2 += robot.location.x;
-                dy2 += robot.location.y;
+            if (robot.type == RobotType.SOLDIER) {
+                cx += robot.location.x * s_attraction;
+                cy += robot.location.y * s_attraction;
+                // absolute value
+                if ((Math.abs(robot.location.x - loc.x) + Math.abs(robot.location.y - loc.y)) < 2) {
+                    dx2 -= Math.abs(robot.location.x - loc.x) * s_repulsion;
+                    dy2 -= Math.abs(robot.location.y - loc.y) * s_repulsion;
+                }
+            }
+            else if (robot.type == RobotType.MINER) {
+                cx += robot.location.x * m_attraction;
+                cy += robot.location.y * m_attraction;
+                // absolute value
+                if ((Math.abs(robot.location.x - loc.x) + Math.abs(robot.location.y - loc.y)) < 2) {
+                    dx2 -= Math.abs(robot.location.x - loc.x) * m_repulsion;
+                    dy2 -= Math.abs(robot.location.y - loc.y) * m_repulsion;
+                }
             }
         }
         cx /= num;
         cy /= num;
-        float dx1 = (cx - (float) loc.x) * attraction;
-        float dy1 = cy - (float) loc.y * attraction;
+        double dx1 = (cx - (double) loc.x);
+        double dy1 = cy - (double) loc.y;
+        double dx = dx1 + dx2;
+        double dy = dy1 + dy2;
+        // convert dx and dy to direction
+        // values are derived from tangent of 22.5 and 67.5
+        if (dy > 0) {
+            if (dy > 2.4 * Math.abs(dx)) {
+                d = Direction.NORTH;
+            }
+            else if (dy > 0.4 * Math.abs(dx)) {
+                if (dx > 0) {
+                    d = Direction.NORTHEAST;
+                }
+                else {
+                    d = Direction.NORTHWEST;
+                }
+            }
+            else {
+                if (dx > 0) {
+                    d = Direction.EAST;
+                }
+                else {
+                    d = Direction.WEST;
+                }
+            }
+        }
+        else {
+            if (dy < -2.4 * Math.abs(dx)) {
+                d = Direction.SOUTH;
+            }
+            else if (dy < -0.4 * Math.abs(dx)) {
+                if (dx > 0) {
+                    d = Direction.SOUTHEAST;
+                }
+                else {
+                    d = Direction.SOUTHWEST;
+                }
+            }
+        }
+        return d;
     }
 
     public void attemptAttack() throws GameActionException {
