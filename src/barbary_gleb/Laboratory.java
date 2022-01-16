31d150a7e6e0b0082f77510c28a632f71dc4cfa7
Claeb101 <calebarulandu@gmail.com>
2022-01-16 17:55:07 -0500

    add early movement for archons

---
+++ src/barbary_gleb/Laboratory.java
@@ -0,0 +1,12 @@
+package barbary_gleb;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class Laboratory extends Unit {
+	public Laboratory(RobotController rc) throws GameActionException {
+        super(rc);
+    }
+
+    public void run() throws GameActionException {}
+}
