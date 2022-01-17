c43a462e5db51bc403c33bb61ea729998b9d69c9
CyrilSharma <sharma.cyril@gmail.com>
2022-01-16 22:42:21 -0500

    add bot rebutia for merging changes

---
+++ src/rebutia/Laboratory.java
@@ -0,0 +1,12 @@
+package rebutia;
+
+import battlecode.common.*;
+import java.util.*;
+
+public class Laboratory extends Unit {
+	public Laboratory(RobotController rc) throws GameActionException {
+        super(rc);
+    }
+
+    public void run() throws GameActionException {}
+}
