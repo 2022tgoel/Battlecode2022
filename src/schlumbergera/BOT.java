b19867d2865df4802dd56796e7c3a36980445a51
2022tgoel <tishi.tng@gmail.com>
2022-01-21 18:39:00 -0500

    chooses archon movement location

---
+++ src/schlumbergera/BOT.java
@@ -0,0 +1,16 @@
+package schlumbergera;
+
+public enum BOT {
+    NONE(0),
+    MINER(1),
+    SOLDIER(2),
+    BUILDER(3),
+    SAGE(4),
+    WATCHTOWER(5),
+    LABORATORY(6),
+    ;
+
+    private final int id;
+    BOT(int id) { this.id = id; }
+    public int getValue() { return id; }
+}
