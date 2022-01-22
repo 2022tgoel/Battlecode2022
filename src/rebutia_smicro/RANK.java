c2bfb8e530ce13770a0e7f2fb2d92c13cab605d8
CyrilSharma <sharma.cyril@gmail.com>
2022-01-22 10:42:56 -0500

    new bot for soldier micro

---
+++ src/rebutia_smicro/RANK.java
@@ -0,0 +1,11 @@
+package rebutia_smicro;
+
+public enum RANK { 
+    DEFAULT(0),
+    DEFENDER(1),
+    MARTYR(2);
+
+    private final int id;
+    RANK(int id) { this.id = id; }
+    public int getValue() { return id; }
+}
