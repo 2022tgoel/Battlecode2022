ba88316c5c09fc8c6c3dad78c3ca1ea20bc9cd3e
CyrilSharma <sharma.cyril@gmail.com>
2022-01-23 11:26:53 -0500

    added bot oputia for merge

---
+++ src/opuntia/CHANNEL.java
@@ -0,0 +1,36 @@
+package opuntia;
+
+public enum CHANNEL {
+    ROUND_NUM(0),
+    MINERS_ALIVE(1),
+    SOLDIERS_ALIVE(2),
+    BUILDERS_ALIVE(3),
+    TOWERS_ALIVE(4),
+    MINERS_ALIVE_ALT(5),
+    SOLDIERS_ALIVE_ALT(6),
+    BUILDERS_ALIVE_ALT(7),
+    TOWERS_ALIVE_ALT(8),
+    USEFUL_MINERS(9),
+    USEFUL_MINERS_ALT(10),
+
+    LEAD_ESTIMATE(19),
+    ARCHON_LOC_1(20),
+    fARCHON_STATUS1(24),
+    ARCHON_NUMBER(28),
+    UNIT_BUILT(29),
+
+    MINING1(30),
+    TARGET(35),
+    ARCHON_MODE(40),
+
+    ORDERS(61),
+    SEND_RANKS1(62),
+    SEND_RANKS2(63), 
+    ;
+    
+    public static final int NUM_TARGETS = 5;
+
+    private final int id;
+    CHANNEL(int id) { this.id = id; }
+    public int getValue() { return id; }
+}
---
+++ src/opuntia/MinerNav.java
@@ -0,0 +1,87 @@
+package opuntia;
+
+import battlecode.common.*;
+public class MinerNav {
+    static RobotController rc;
+    MinerNav(RobotController rc) {
+        this.rc = rc;
+    }
+    static MapLocation loc0;
+    static MapLocation loc1;
+    static MapLocation loc2;
+    static MapLocation loc3;
+    static MapLocation loc4;
+    static MapLocation loc5;
+    static MapLocation loc6;
+    static MapLocation loc7;
+    static MapLocation bestSpot;
+    int rubble;
+    MapLocation findBestSquare(MapLocation leadLoc, int minRubble) throws GameActionException{
+        loc0 = leadLoc.add(Direction.EAST);
+        loc1 = leadLoc.add(Direction.WEST);
+        loc2 = leadLoc.add(Direction.SOUTH);
+        loc3 = leadLoc.add(Direction.NORTH);
+        loc4 = leadLoc.add(Direction.NORTHEAST);
+        loc5 = leadLoc.add(Direction.NORTHWEST);
+        loc6 = leadLoc.add(Direction.SOUTHEAST);
+        loc7 = leadLoc.add(Direction.SOUTHWEST);
+        if (rc.canSenseLocation(loc0)) {
+            rubble = 1 + rc.senseRubble(loc0) / 10;
+            if (rubble < minRubble) {
+                minRubble = rubble;
+                bestSpot = loc0;
+            }
+        }
+        if (rc.canSenseLocation(loc1)) {
+            rubble = 1 + rc.senseRubble(loc1) / 10;
+            if (rubble < minRubble) {
+                minRubble = rubble;
+                bestSpot = loc1;
+            }
+        }
+        if (rc.canSenseLocation(loc2)) {
+            rubble = 1 + rc.senseRubble(loc2) / 10;
+            if (rubble < minRubble) {
+                minRubble = rubble;
+                bestSpot = loc2;
+            }
+        }
+        if (rc.canSenseLocation(loc3)) {
+            rubble = 1 + rc.senseRubble(loc3) / 10;
+            if (rubble < minRubble) {
+                minRubble = rubble;
+                bestSpot = loc3;
+            }
+        }
+        if (rc.canSenseLocation(loc4)) {
+            rubble = 1 + rc.senseRubble(loc4) / 10;
+            if (rubble < minRubble) {
+                minRubble = rubble;
+                bestSpot = loc4;
+            }
+        }
+        if (rc.canSenseLocation(loc5)) {
+            rubble = 1 + rc.senseRubble(loc5) / 10;
+            if (rubble < minRubble) {
+                minRubble = rubble;
+                bestSpot = loc5;
+            }
+        }
+        if (rc.canSenseLocation(loc6)) {
+            rubble = 1 + rc.senseRubble(loc6) / 10;
+            if (rubble < minRubble) {
+                minRubble = rubble;
+                bestSpot = loc6;
+            }
+        }
+        if (rc.canSenseLocation(loc7)) {
+            rubble = 1 + rc.senseRubble(loc7) / 10;
+            if (rubble < minRubble) {
+                minRubble = rubble;
+                bestSpot = loc7;
+            }
+        }
+        return bestSpot;
+    }
+}
+
