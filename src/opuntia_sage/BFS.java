23bba1ffc8a241c15a1fcac6548692e9982bd313
2022tgoel <tishi.tng@gmail.com>
2022-01-27 22:03:04 -0500

    rm old opuntia

--- src/opuntia_sage/BFS.java
+++
@@ -1,8 +0,0 @@
-package opuntia_sage;
-
-import battlecode.common.Direction;
-import battlecode.common.GameActionException;
-import battlecode.common.MapLocation;
-public abstract class BFS {
-	abstract Direction getBestDir(MapLocation target) throws GameActionException;
-}
